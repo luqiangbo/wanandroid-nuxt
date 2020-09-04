95f38c58e7afacb999a30a71cddf3822b6e0e8cd
caniuse <luqiangbovip@163.com>
2020-09-04 23:20:27 +0800

    去除https配置

--- v1/nuxt.config.js
+++ v1/nuxt.config.js
@@ -1,6 +1,3 @@
-import path from 'path'
-import fs from 'fs'
-
 const myApiEnv = process.env.MY_API_ENV
 let baseURL = 'https://www.wanandroid.com/'
 if (myApiEnv === 'local') {
@@ -20,12 +17,12 @@ export default {
   server: {
     port: 3001, // default: 3000
     host: '0.0.0.0', // default: localhost
-    server: {
-      https: {
-        key: fs.readFileSync(path.resolve(__dirname, '/root/nginx/ssl/key.pem')),
-        cert: fs.readFileSync(path.resolve(__dirname, '/root/nginx/ssl/cert.pem')),
-      },
-    },
+    // server: {
+    //   https: {
+    //     key: fs.readFileSync(path.resolve(__dirname, '/root/nginx/ssl/key.pem')),
+    //     cert: fs.readFileSync(path.resolve(__dirname, '/root/nginx/ssl/cert.pem')),
+    //   },
+    // },
   },
   render: {
     compressor: false, // 关闭默认gzip打包
